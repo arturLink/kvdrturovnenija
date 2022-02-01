033399ab4a8c7627220ce7fbf545e885e2d0d81e
opilane <opilane@HP-CZC6297BBB>
2022-02-01 13:55:58 +0200

    bunda

--- kvdrturovnenija.py
+++ kvdrturovnenija.py
@@ -113,7 +113,28 @@ def kala():
     plt.grid(True)
     plt.show()
 def serdtse(): pass
-def vihmavari(): pass
+def o4ki():
+    x1 = np.arange(-9,-0.5,0.5)
+    y1= -(1/16)*(x1+5)**2+2
+    x2 = np.arange(1,9.5,0.5)
+    y2= -(1/16)*(x2-5)**2+2
+    x3 = np.arange(-9,-0.5,0.5)
+    y3= (1/4)*(x3+5)**2-3
+    x4 = np.arange(1,9.5,0.5)
+    y4= (1/4)*(x4-5)**2-3
+    x5 = np.arange(-9,-5.5,0.5)
+    y5= -(x5+7)**2+5
+    x6 = np.arange(6,9.5,0.5)
+    y6= -(x6-7)**2+5
+    x7 = np.arange(-1,1.5,0.5)
+    y7= -(0.5)*x7**2+1.5
+    fig = plt.figure()
+    plt.plot(x1, y1,x2,y2,x3,y3,x4,y4,x5,y5,x6,y6,x7,y7)
+    plt.title('очко)')
+    plt.ylabel('y')
+    plt.xlabel('x')
+    plt.grid(True)
+    plt.show()
 def konn(): pass
 def figura():
     global var
@@ -121,7 +142,7 @@ def figura():
     if valik==1:
         kala()
     elif valik==2:
-        vihmavari()
+        o4ki()
     else:
         konn()
 
